aef401dc3d5a6e33f4461475510699a5f40c253a
Merrill-Jade <mj.kim1102@gmail.com>
2020-06-19 17:11:19 +0900

    Updates

---
+++ precache-manifest.0a9be9bdf42d98865e524d0030146064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a9e8f8670851f6c77e26d188096c01",
+    "url": "/react_web_prac_app/index.html"
+  },
+  {
+    "revision": "dd73a37debf30a176f21",
+    "url": "/react_web_prac_app/static/css/main.146933bf.chunk.css"
+  },
+  {
+    "revision": "7ccd3a7902adc90b540f",
+    "url": "/react_web_prac_app/static/js/2.875c4c91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_web_prac_app/static/js/2.875c4c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd73a37debf30a176f21",
+    "url": "/react_web_prac_app/static/js/main.7940d276.chunk.js"
+  },
+  {
+    "revision": "3e2d22fe856c02d393e7",
+    "url": "/react_web_prac_app/static/js/runtime-main.2494342f.js"
+  }
+]);
